fca1ca6be8e35a7b18c74513b5476a6eacdc569a
Tatsunori Uchino <tats.u@live.jp>
2021-03-16 00:45:53 +0900

    Enable bundling

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { terser } from "rollup-plugin-terser";
+// import typescript from 'rollup-plugin-typescript';
+
+const commonjs_plugin = commonjs({
+  dynamicRequireTargets: [
+    "node_modules/unicode-regex/lib/data.generated/*/*.json",
+  ],
+});
+
+export default {
+  input: "src/index.js",
+  plugins: [resolve(), commonjs_plugin, json(), terser() /* , typescript() */],
+  external: ["prettier"],
+  output: {
+    file: "dist/main.js",
+    format: "cjs",
+    sourcemap: true,
+  },
+};
